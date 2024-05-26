2ea9639f614583e4ab472619c9fe52dfbbc28906
Mark Nenadov <marknenadov@gmail.com>
2024-05-26 08:36:00 -0400

    reduce duplication in query_all_observations

--- src/thirty_grand/queries/__init__.py
+++ src/thirty_grand/queries/__init__.py
@@ -1,24 +1,21 @@
+from typing import Optional
+
+import pandas as pd
+
 from .. import observation
 
 
+def _name_matches(partial: Optional[str], full_name: str) -> bool:
+    return partial is None or (isinstance(full_name, str) and partial.lower() in full_name.lower())
+
+
 def query_all_observations(
-        data,
-        scientific_name_partial=None,
-        common_name_partial=None
+        data: pd.DataFrame,
+        scientific_name_partial: Optional[str] = None,
+        common_name_partial: Optional[str] = None
 ) -> [observation.Observation]:
     observations = []
-
     for _, row in data.iterrows():
-        row_common_name = row['common_name'] or ""
-        has_row_common_name = isinstance(row_common_name, str) and row_common_name != ""
-        has_common_name_partial = common_name_partial is not None
-        common_name_match = has_common_name_partial and has_row_common_name and common_name_partial.lower() in row_common_name.lower()
-
-        row_scientific_name = row['scientific_name'] or ""
-        has_row_scientific_name = isinstance(row_scientific_name, str) and row_scientific_name != ""
-        has_scientific_name_partial = scientific_name_partial is not None
-        scientific_name_match = has_scientific_name_partial and has_row_scientific_name and scientific_name_partial.lower() in row_scientific_name.lower()
-
-        if (scientific_name_partial is None or scientific_name_match) and (common_name_partial is None or common_name_match):
+        if _name_matches(common_name_partial, row['common_name']) and _name_matches(scientific_name_partial, row['scientific_name']):
             observations.append(observation.Observation.create_from_row(row))
     return observations
