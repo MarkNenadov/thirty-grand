a9f486ce47663634631b3c481b4417a19bb1b5c9
Mark Nenadov <marknenadov@gmail.com>
2024-05-31 06:45:42 -0400

    docstrings for query methods

--- src/thirty_grand/queries/__init__.py
+++ src/thirty_grand/queries/__init__.py
@@ -15,12 +15,35 @@ def query_all_observations(
         scientific_name_partial: Optional[str] = None,
         common_name_partial: Optional[str] = None
 ) -> [observation.Observation]:
+    """
+    Query all observations from a DataFrame returned from csv. Optionally, match the given partial
+    scientific and common names.
+
+    Args:
+        data (pd.DataFrame): The DataFrame containing observation csv data.
+        scientific_name_partial (Optional[str]): Partial latin/scientific name to filter observations.
+        common_name_partial (Optional[str]): Partial common name to filter observations.
+
+    Returns:
+        List[observation.Observation]: A list of Observation objects that match the given partial names.
+    """
     observations = []
     for _, row in data.iterrows():
-        if _name_matches(common_name_partial, row['common_name']) and _name_matches(scientific_name_partial, row['scientific_name']):
+        if _name_matches(common_name_partial, row['common_name']) and \
+                _name_matches(scientific_name_partial, row['scientific_name']):
             observations.append(observation.Observation.create_from_row(row))
     return observations
 
 
 def random_sample(data: pd.DataFrame, count: int):
+    """
+    Retrieve a random sample of observations from the DataFrame.
+
+    Args:
+        data (pd.DataFrame): The DataFrame containing observation csv data.
+        count (int): The number of random observations to retrieve.
+
+    Returns:
+        List[observation.Observation]: A random sample of Observation objects.
+    """
     return random.sample(query_all_observations(data), count)
