ff45e84a371a3465f739863b857784b9be34c6ab
Mark Nenadov <marknenadov@gmail.com>
2024-07-07 16:09:03 -0400

    add utility functions relevant to plotting yearly observation counts

--- src/thirty_grand/utilities/__init__.py
+++ src/thirty_grand/utilities/__init__.py
@@ -1,5 +1,8 @@
 from prettytable import PrettyTable
 from .. import observation
+from collections import Counter
+import datetime
+import matplotlib.pyplot as pyplot
 
 
 def get_observations_table_str(observations: [observation.Observation]):
@@ -31,5 +34,32 @@ def get_observations_table_str(observations: [observation.Observation]):
     return table.get_string()
 
 
-def print_observations_table(observations: [observation.Observation]):
+def print_observations_table(observations: [observation.Observation]) -> None:
     print(get_observations_table_str(observations))
+
+
+def display_yearly_observation_barchart(observations: [observation.Observation]) -> None:
+    years, numbers = extract_year_observation_counts(observations)
+
+    pyplot.bar(years, numbers, color='skyblue')
+
+    pyplot.title('Number of Observations Per Year')
+    pyplot.xlabel('Year')
+    pyplot.ylabel('Number of Observations')
+
+    pyplot.xticks(years)
+
+    pyplot.show()
+
+
+def extract_years(observations: [observation.Observation]) -> []:
+    return [datetime.datetime.strptime(obs.observed_on, "%Y-%m-%d").year for obs in observations]
+
+
+def extract_year_observation_counts(observations: [observation.Observation]):
+    year_counts = Counter(extract_years(observations))
+
+    years = list(year_counts.keys())
+    numbers = list(year_counts.values())
+
+    return [years, numbers]
