c53c96e529c78e5a3822ad76a727d1abde25895d
Mark Nenadov <marknenadov@gmail.com>
2024-07-14 07:15:58 -0400

    doc string for read_csv

--- src/thirty_grand/utilities/csv/__init__.py
+++ src/thirty_grand/utilities/csv/__init__.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
 
-def read_csv(file_path):
-    result = pd.read_csv(file_path, low_memory=False)
-    result.fillna('')
+def read_csv(file_path: str) -> pd.DataFrame:
+    """
+    Reads a CSV file into a pandas DataFrame and fills NA/NaN values with an empty string.
 
-    return result
+    Parameters:
+        file_path: Full path to CSV file
+    """
+    return pd.read_csv(file_path, low_memory=False).fillna('')
