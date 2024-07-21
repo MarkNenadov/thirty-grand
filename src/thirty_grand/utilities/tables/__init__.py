1007d913a6887c9255117e225868d3f03c2644bd
Mark Nenadov <marknenadov@gmail.com>
2024-07-21 13:48:38 -0400

    implemented way for print_family_table to filter by order or any other taxonomic name

--- src/thirty_grand/utilities/tables/__init__.py
+++ src/thirty_grand/utilities/tables/__init__.py
@@ -34,17 +34,18 @@ def get_observations_table_str(observations: [observation.Observation]):
     return table.get_string()
 
 
-def get_property_counts(observations, property_name):
+def get_property_counts(observations, property_name, filter_property = None, filter_by_value = None):
     property_counts = defaultdict(int)
     for obs in observations:
         property_value = getattr(obs, property_name, "")
-        if property_value != "":
+        filter_value = getattr(obs, filter_property, "") if filter_property and filter_by_value else ""
+        if property_value != "" and (filter_by_value is None or filter_value == filter_by_value):
             property_counts[property_value] += 1
     return dict(property_counts)
 
 
 def get_class_table_str(observations: [observation.Observation], threshold: int):
-    class_counts_dict = get_property_counts(observations, "class_name")
+    class_counts_dict = get_property_counts(observations, "class_name", None, None)
     sorted_class_counts = sorted(class_counts_dict.items(), key=lambda item: item[1], reverse=True)
 
 
@@ -65,12 +66,12 @@ def get_class_table_str(observations: [observation.Observation], threshold: int)
     return table.get_string()
 
 
-def get_taxon_table_str(observations: [observation.Observation], threshold: int, taxon_property_name: str):
+def get_taxon_table_str(observations: [observation.Observation], threshold: int, taxon_property_name: str, filter_property: str = None, filter_value: str = None):
     assert taxon_property_name is not None, "get_taxon_table_str requires taxon_property_name"
     assert taxon_property_name.endswith("_name"), "get_taxon_table_str requires taxon_property_name ending with '_name'"
 
-    taxon_counts_dict = get_property_counts(observations, taxon_property_name)
-    sorted_txxon_counts = sorted(taxon_counts_dict.items(), key=lambda item: item[1], reverse=True)
+    taxon_counts_dict = get_property_counts(observations, taxon_property_name, filter_property, filter_value)
+    sorted_taxon_counts = sorted(taxon_counts_dict.items(), key=lambda item: item[1], reverse=True)
 
 
     table = PrettyTable()
@@ -79,7 +80,7 @@ def get_taxon_table_str(observations: [observation.Observation], threshold: int,
         "Number of Observations",
     ]
 
-    for taxon_name, taxon_name_count in sorted_txxon_counts:
+    for taxon_name, taxon_name_count in sorted_taxon_counts:
         if taxon_name_count >= threshold:
             table.add_row(
                 [
@@ -97,8 +98,10 @@ def print_observations_table(observations: [observation.Observation]) -> None:
 def print_class_table(observations: [observation.Observation], threshold: int) -> None:
     print(get_taxon_table_str(observations, threshold, "class_name"))
 
-def print_family_table(observations: [observation.Observation], threshold: int) -> None:
-    print(get_taxon_table_str(observations, threshold, "family_name"))
 
-def print_order_table(observations: [observation.Observation], threshold: int) -> None:
+def print_family_table(observations: [observation.Observation], threshold: int = 1, filter_property: str = None, filter_value: str = None) -> None:
+    print(get_taxon_table_str(observations, threshold, "family_name", filter_property, filter_value))
+
+
+def print_order_table(observations: [observation.Observation], threshold: int = 1) -> None:
     print(get_taxon_table_str(observations, threshold, "order_name"))
