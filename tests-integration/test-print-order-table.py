5b0b35d39bf11613db9f572956b5beb83581345e
Mark Nenadov <marknenadov@gmail.com>
2024-07-21 13:32:06 -0400

    any integration tests for class/family/order

---
+++ tests-integration/test-print-order-table.py
@@ -0,0 +1,9 @@
+from src.thirty_grand.utilities.tables import print_order_table
+from src.thirty_grand import queries
+from src.thirty_grand.utilities.csv import read_csv
+
+data = read_csv("../data/observations.csv")
+
+all_observations = queries.query_all_observations(data)
+
+print_order_table(all_observations, threshold=300)
