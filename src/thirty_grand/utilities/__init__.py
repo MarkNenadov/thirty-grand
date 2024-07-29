8a09323f953fb04f6f859aa6eabf64a95454657f
Mark Nenadov <marknenadov@gmail.com>
2024-07-29 07:53:44 -0400

    add missing docstrings

--- src/thirty_grand/utilities/__init__.py
+++ src/thirty_grand/utilities/__init__.py
@@ -6,10 +6,22 @@ from src.thirty_grand import observation
 
 
 def extract_years(observations: [observation.Observation]) -> List[int]:
+    """
+    Extract year ints from a list of observations.
+
+    Parameters:
+        observations: The list of observations.
+    """
     return [datetime.datetime.strptime(obs.observed_on, "%Y-%m-%d").year for obs in observations if obs.observed_on != '']
 
 
 def extract_yearly_observation_counts(observations: [observation.Observation]) -> Tuple[List[int], List[int]]:
+    """
+    Extract a tuple of years with their counts from a list of observations.
+
+    Parameters:
+        observations: The list of observations.
+    """
     year_counts = Counter(extract_years(observations))
 
     years, counts = zip(*year_counts.items())
